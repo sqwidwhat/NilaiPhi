9a7b66f9e314e2db09f2ca1ccc183d3785ad5224
sqwidwhat <andikamelawi.net11@gmail.com>
2025-03-25 09:50:59 +0700

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,4 @@
 import javax.swing.*;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         double phi = 4.0;          // Suku pertama: 4
